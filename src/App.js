63a24ffb139af4b18dc3def72e184a7255cfbaca
Jacqueline White <white.jacqueline@heb.com>
2023-06-10 00:37:01 -0500

    hello

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          height="200px"
+          src="https://media.istockphoto.com/id/1196291535/vector/close-up-of-mouth-and-third-eye-and-sunbeams.jpg?s=1024x1024&w=is&k=20&c=6OlnMdevlY4ukqtii1jO_bH-dQLa-s3s0y9_Cfj8Ho4="
+          alt="hello"
+        />
+        <h1>Your Modern Monk </h1>
+        <p>Podcast Coming to you soon</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
