b9ad5d590a6e199629319047d1b6c6fdae10105b
Josh Fletcher <wjfletch26@gmail.com>
2023-06-22 17:10:16 -0500

    new cover image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import "./App.css";
+import "./App.css"
+import modernMonkCover from "./assets/your_modern_monk_cover.png"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img
-          height="200px"
-          src="https://media.istockphoto.com/id/1196291535/vector/close-up-of-mouth-and-third-eye-and-sunbeams.jpg?s=1024x1024&w=is&k=20&c=6OlnMdevlY4ukqtii1jO_bH-dQLa-s3s0y9_Cfj8Ho4="
+          style={{ height: '80vw' }}
+          // src="https://media.istockphoto.com/id/1196291535/vector/close-up-of-mouth-and-third-eye-and-sunbeams.jpg?s=1024x1024&w=is&k=20&c=6OlnMdevlY4ukqtii1jO_bH-dQLa-s3s0y9_Cfj8Ho4="
+          src={modernMonkCover}
           alt="hello"
         />
         <h1>Your Modern Monk </h1>
         <p>Podcast Coming to you soon</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
